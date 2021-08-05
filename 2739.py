862a8455c3247297f485959b5c493836f457727f
oio337a <75519420+oio337a@users.noreply.github.com>
2021-08-05 22:47:09 +0900

    MISSION SUCCESS :)

---
+++ 2739.py
@@ -0,0 +1,6 @@
+# n 단을 입력받고 구구단을 출력하세요.
+
+n = int(input())
+
+for i in range(1,10):
+    print(f"{n} * {i} = {n*i}")
--- 2914.py
+++ 2914.py
@@ -1,4 +1,28 @@
-A, I = map(int, input().split())
+# 밑수의 n 승 법칙
 
-print(type(A))
-# print((I - 1) * A + 1)
+a = []
+b = []
+T = int(input())
+
+for i in range(T):
+    num1, num2 = input().split()
+    a.append(int(num1))
+    b.append(int(num2))
+
+for x in range(T):
+    base = a[x] % 10  # 밑수 구하기
+
+    if base == 0:  # 0인 경우
+        print(10)
+    elif base in [1, 5, 6]:  # 1, 5, 6 인 경우
+        print(base)
+    elif base in [4, 9]:  # 4, 9 인 경우
+        if b[x] % 2 == 0:  # 짝수
+            print(base**2 % 10)
+        else:  # 홀수
+            print(base)
+    else:  # 2, 3, 7, 8 의 경우
+        if b[x] % 4 == 0:
+            print(base**4 % 10)
+        else:
+            print(base**(b[x] % 4) % 10)
