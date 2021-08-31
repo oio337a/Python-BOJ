faab6c7afd30e33dadc40d2ce65201f475a46f7a
oio337a <oio337a@gmail.com>
2021-08-31 18:00:34 +0900

    modified 1244 and MISSION SUCCESS :)

--- 1244.py
+++ 1244.py
@@ -18,23 +18,44 @@ for _ in range(k):
         left = b - 2
         right = b
         while left >= 0 and right < n and li[left] == li[right]:
-            li[left], li[right] = 1, 1 if li[left] == 0 else 0, 0
+            if li[left] == 0:
+                li[left], li[right] = 1, 1
+            elif li[left] == 1:
+                li[left], li[right] = 0, 0
+            # ValueError: too many values to unpack
+            # li[left], li[right] = 1, 1 if li[left] == 0 else 0, 0
             left -= 1
             right += 1
             if left < 0 or right >= n:
                 break
+        # 안된다.. 인덱스 에러
+        # for j in range(1, len(li)//2):
+        #     if b-j-1 < 0 or b+j-1 >= n:
+        #         break
+        #     if b-j-1 >= 0 b+j-1 < n
+        #     if li[b-j-1] == li[b+j-1]:
+        #         li[b-j-1] = 0 if li[b-j-1] == 1 else 1
+        #         li[b+j-1] = 0 if li[b+j-1] == 1 else 1
 
-# cnt = 0
-# ans = ''
-# for i in range(n):
-#     ans += (str(li[i]) + ' ')
-#     cnt += 1
-#     if cnt == 20:
-#         print(ans)
-#         ans = ''
-#         cnt = 0
-# if len(ans) != 0:
-#     print(ans)
+cnt = 0
+ans = ''
+for i in range(n):
+    ans += (str(li[i]) + ' ')
+    cnt += 1
+    if cnt == 20:
+        print(ans)
+        ans = ''
+        cnt = 0
+if len(ans) != 0:
+    print(ans)
 
-for idx in range(1, len(li), 20):
-    print(*li[idx:idx+20])
+# for idx in range(1, len(li), 20):
+#     print(*li[idx:idx+20])
+'''
+생각해 볼것 
+0 또는 1 을 변경하는 문제
+li[i] = (li[i]+1) % 2
+li[i]가 0 이면 1
+li[i]가 1 이면 0
+으로 변경
+'''
