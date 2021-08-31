ad3062ce15166ff1fb5f3ee82b98f660c426ab0f
oio337a <oio337a@gmail.com>
2021-08-31 16:37:29 +0900

    MISSION SUCCESS:)

--- 1244.py
+++ 1244.py
@@ -5,25 +5,36 @@ li = list(map(int, input().split()))
 
 k = int(input())
 
-SPLITS = 20
 for _ in range(k):
+    # a = 성별
+    # b = 변경할 인덱스 + 1
     a, b = map(int, input().split())
-    if a == 1:
+    if a == 1:  # 남성일때
         for i in range(1, (len(li) // b) + 1):
-            b *= i
-            if li[b - 1] == 1:
-                li[b - 1] = 0
-            else:
-                li[b - 1] = 1
-        # 0, 1, 1, 1, 0, 1, 0, 1
+            q = (b * i) - 1
+            li[q] = 0 if li[q] == 1 else 1
     else:
         li[b-1] = 0 if li[b-1] == 1 else 1
-        for j in range(1, len(li)//2):
-            if b-j-1 < 0 or b+j-1 >= n:
+        left = b - 2
+        right = b
+        while left >= 0 and right < n and li[left] == li[right]:
+            li[left], li[right] = 1, 1 if li[left] == 0 else 0, 0
+            left -= 1
+            right += 1
+            if left < 0 or right >= n:
                 break
-            if li[b-j-1] == li[b+j-1]:
-                li[b-j-1] = 0 if li[b-j-1] == 1 else 1
-                li[b+j-1] = 0 if li[b+j-1] == 1 else 1
-        # [1, 0, 1, 0, 0, 1, 0, 1]
-for idx in range(0, len(li), SPLITS):
-    print(*li)
+
+# cnt = 0
+# ans = ''
+# for i in range(n):
+#     ans += (str(li[i]) + ' ')
+#     cnt += 1
+#     if cnt == 20:
+#         print(ans)
+#         ans = ''
+#         cnt = 0
+# if len(ans) != 0:
+#     print(ans)
+
+for idx in range(1, len(li), 20):
+    print(*li[idx:idx+20])
