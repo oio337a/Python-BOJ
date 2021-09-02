1e9a0d41f6bf0942ed1d7db7b1c23df3cf180369
oio337a <oio337a@gmail.com>
2021-09-02 22:38:40 +0900

    MISSION SUCCESS :)

---
+++ 17608.py
@@ -0,0 +1,16 @@
+import sys
+N = int(sys.stdin.readline())
+
+li = []
+for _ in range(N):
+    li.append(int(sys.stdin.readline()))
+
+# li.reverse()
+cnt = 0
+N = li[-1]
+for i in li[::-1]:
+    print(i)
+    if i > N:
+        cnt += 1
+        N = i
+print(cnt+1)
