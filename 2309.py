22bc306c80ef238d6d7e0d73d656c32a38b53591
oio337a <oio337a@gmail.com>
2021-09-06 15:39:22 +0900

    MISSION SUCCESS :)

---
+++ 2309.py
@@ -0,0 +1,19 @@
+import sys
+import itertools
+
+li = []
+for _ in range(9):
+    li.append(int(sys.stdin.readline().rstrip()))
+
+li.sort()
+
+seven = list(itertools.combinations(li, 7))
+
+answer = []
+for i in seven:
+    if sum(i) == 100:
+        answer = sorted(i)
+        break
+
+for _ in answer:
+    print(_)
