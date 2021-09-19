1f1e0b3caa6836999524aaf79b9e7c94a3981fc8
oio337a <oio337a@gmail.com>
2021-09-19 16:58:15 +0900

    mission success

---
+++ 10799.py
@@ -0,0 +1,18 @@
+from typing import AnyStr
+
+
+bar = list(input())
+answer = 0
+stack = []
+
+for i in range(len(bar)):
+    if bar[i] == '(':
+        stack.append('(')
+    else:
+        if bar[i-1] == '(':
+            stack.pop()
+            answer += len(stack)
+        else:
+            stack.pop()
+            answer += 1
+print(answer)
