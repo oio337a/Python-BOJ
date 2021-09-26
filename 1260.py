b8b3e8642377749f19477ee6a8edc359c86a7682
oio337a <oio337a@gmail.com>
2021-09-26 14:25:35 +0900

    mission successs

--- 1260.py
+++ 1260.py
@@ -1,42 +1,39 @@
-
+from collections import deque
 import sys
 input = sys.stdin.readline
 
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visit_list[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=" ")
+        for i in range(1, n + 1):
+            if visit_list[i] == 0 and graph[v][i] == 1:
+                q.append(i)
+                visit_list[i] = 1
+
+
+def dfs(v):
+    visit_list2[v] = 1
+    print(v, end=" ")
+    for i in range(1, n+1):
+        if visit_list2[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+
+
 n, m, v = map(int, input().split())
 
-dic = {}
-for i in range(n):
-    dic[i+1] = set()
-for j in range(m):
+graph = [[0] * (n + 1) for _ in range((n+1))]
+visit_list = [0] * (n+1)
+visit_list2 = [0]*(n+1)
+
+for _ in range(m):
     a, b = map(int, input().split())
-    dic[a].add(b)
-    dic[b].add(a)
+    graph[a][b] = graph[b][a] = 1
 
-# 정렬이 안되서..
-# DFS
-
-
-def dfs(start, dic):
-    for i in dic[start]:
-        if i not in visited_0:
-            visited_0.append(i)
-            dfs(i, dic)
-
-# BFS
-
-
-def bfs(s, dic):
-    que = [s]
-    while que:
-        for i in dic[que.pop()]:
-            if i not in visited_1:
-                visited_1.append(i)
-                que.append(i)
-
-
-visited_0 = [v]
-visited_1 = [v]
-dfs(v, dic)
-bfs(v, dic)
-print(*visited_0)
-print(*visited_1)
+dfs(v)
+print()
+bfs(v)
