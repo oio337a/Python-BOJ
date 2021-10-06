7aa8a9c867a581afb00399835935b587fc0326d1
oio337a <oio337a@gmail.com>
2021-10-06 17:26:08 +0900

    holimoli

---
+++ 11723.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = set()
+
+for _ in range(n):
+    temp = input().split()
+
+    if len(temp) == 1:  # 커멘드만 입력 받았다면
+        if temp[0] == 'all':
+            s = set([i for i in range(1, 21)])
+        else:
+            s = set()
+
+    else:
+        co, num = temp[0], temp[1]
+        num = int(num)
+
+        if co == 'add':
+            li.add(num)
+        elif co == 'remove':
+            li.discard(num)
+        elif co == 'check':
+            print(1 if num in li else 0)
+        elif co == 'toggle':
+            if num in li:
+                li.discard(num)
+            else:
+                li.add(num)
