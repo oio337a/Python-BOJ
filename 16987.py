b051d484128b80cdb53ee3e081b99aaebb2b24cc
oio337a <oio337a@gmail.com>
2021-10-22 14:05:27 +0900

    GOOD:)

---
+++ 16987.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = [0] * n
+w = [0] * n
+for i in range(n):
+    s[i], w[i] = map(int, input().split())
+
+ans = 0
+
+
+def rec_egg(idx, eggs):
+    global ans
+    if idx == n:
+        cnt = 0
+        for i in range(n):
+            if eggs[i] <= 0:
+                cnt += 1
+        if cnt > ans:
+            ans = cnt
+        return
+
+    if eggs[idx] > 0:
+        for i in range(n):
+            flag = False
+            if eggs[i] > 0 and i != idx:
+                flag = True
+                temp = eggs[:]
+                temp[i] -= w[idx]
+                temp[idx] -= w[i]
+                rec_egg(idx+1, temp)
+        if not flag:
+            rec_egg(idx+1, eggs)
+    else:
+        rec_egg(idx+1, eggs)
+
+
+rec_egg(0, s)
+print(ans)
