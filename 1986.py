bcdfa2efdc0bb0699ff1e63e6a95b20b4727d203
oio337a <oio337a@gmail.com>
2021-12-10 21:29:06 +0900

    PS

---
+++ 1986.py
@@ -0,0 +1,66 @@
+import sys
+import collections
+input = sys.stdin.readline
+
+kdi = [-1, 1, -2, -2, -1, 1, 2, 2]
+kdj = [-2, -2, -1, 1, 2, 2, -1, 1]
+
+
+def move_k(i, j):
+    global t
+    for a in range(8):
+        ni, nj = i + kdi[a], j + kdj[a]
+        if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj]:
+            visited[ni][nj] = 1
+            t -= 1
+
+
+qdi = [-1, -1, -1, 0, 1, 1, 1, 0]
+qdj = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+
+def move_q(i, j):
+    global t
+    for a in range(8):
+        ni = i
+        nj = j
+        while True:
+            ni, nj = ni+qdi[a], nj+qdj[a]
+            if 0 <= ni < n and 0 <= nj < m:
+                if visited[ni][nj] == 0:
+                    visited[ni][nj] = 1
+                    t -= 1
+                elif visited[ni][nj] == 2:
+                    break
+            else:
+                break
+
+
+'''초기 세팅'''
+n, m = map(int, input().split())
+visited = [[0]*m for _ in range(n)]
+t = n * m
+q = collections.deque()
+k = collections.deque()
+
+"""보드 입력"""
+for i in range(3):
+    s = collections.deque(input().split())
+    for j in range(int(s.popleft())):
+        a = int(s.popleft()) - 1
+        b = int(s.popleft()) - 1
+        visited[a][b] = 2
+        t -= 1
+        if i == 0:
+            q.append([a, b])
+        elif i == 1:
+            k.append([a, b])
+
+while k:
+    i, j = k.popleft()
+    move_k(i, j)
+while q:
+    i, j = q.popleft()
+    move_q(i, j)
+
+print(t)
