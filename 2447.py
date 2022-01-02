8cfbcd045222b924babfcdd16f465708911f0970
oio337a <oio337a@gmail.com>
2022-01-02 17:59:40 +0900

    m

--- 2447.py
+++ 2447.py
@@ -6,13 +6,16 @@ def counting_star(n):
         board[0][:3] = board[2][:3] = ['*']*3
         board[1][:3] = ['*', ' ', '*']
         return
+        # ***
+        # * *
+        # ***
 
-    a = n // 3
-    counting_star(a)
-    for i in range(3):
-        for j in range(3):
+    a = n // 3  # 3으로 나눈 구역
+    counting_star(a)  # 3
+    for i in range(3):  # 0 1 2
+        for j in range(3):  # 0 1 2
             if i == 1 and j == 1:
-                continue
+                continue   # 가운데 영역 건너 뛰기
             for k in range(a):
                 board[a*i+k][a*j:a*(j+1)] = board[k][:a]
 
