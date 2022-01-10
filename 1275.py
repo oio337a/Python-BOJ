e8f0f590545529d7bf3107c2b77596845d4e52a1
oio337a <oio337a@gmail.com>
2022-01-10 20:08:51 +0900

    PS

---
+++ 1275.py
@@ -0,0 +1,51 @@
+from math import e
+import sys
+
+input = sys.stdin.readline
+
+
+def init(start, end, node):
+    if start == end:
+        tree[node] = li[start]
+        return tree[node]
+    mid = (start + end) // 2
+    tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1)
+    return tree[node]
+
+
+def summit(start, end, node, left, right):
+    if left > end or right < start:
+        return 0
+
+    if left <= start and end <= right:
+        return tree[node]
+
+    mid = (start + end) // 2
+    return summit(start, mid, node * 2, left, right) + summit(
+        mid + 1, end, node * 2 + 1, left, right
+    )
+
+
+def update(start, end, node, index, diff):
+    if index < start or index > end:
+        return
+    tree[node] += diff
+    if start == end:
+        return
+    mid = (start + end) // 2
+    update(start, mid, node * 2, index, diff)
+    update(mid + 1, end, node * 2 + 1, index, diff)
+
+
+n, q = map(int, input().split())
+li = list(map(int, input().split()))
+tree = [0] * (n * 4)
+
+init(0, n - 1, 1)
+
+for _ in range(q):
+    x, y, a, b = map(int, input().split())
+    print(summit(0, n - 1, 1, x - 1, y - 1))
+    diff = b - li[a - 1]
+    li[a - 1] = b
+    update(0, n - 1, 1, a, diff)
--- test.py
+++ test.py
@@ -1,33 +1,62 @@
-from collections import deque
+# from collections import deque
 
 
-def solution(board):
-    n = len(board)
-    answer = int(1e9)
-    dp = [[int(1e9) for _ in range(n)] for _ in range(n)]
-    # 현재 지나간 길을 확인하기 위한 idx 추가
-    directions = [(-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)]
-    # i, j, cost, direction
-    q = deque([(0, 0, 0, -1)])
-    while q:
-        i, j, cost, dir_idx = q.popleft()
-        # 정답 범위이고, 현재 cost가 더 적을 때 값 할당
-        if (i, j) == (n - 1, n - 1) and answer > cost:
-            answer = cost
-        for direction in directions:
-            # 다음 값 셋팅
-            next_i = i + direction[0]
-            next_j = j + direction[1]
-            add_cost = 1 if dir_idx == direction[2] or dir_idx == -1 else 6
-            # 현재 값 판단할 지 여부
-            if not (0 <= next_i < n and 0 <= next_j < n) or board[next_i][next_j] == 1:
-                continue
-            if dp[next_i][next_j] < cost + add_cost:
-                continue
-            # dp에 값 설정 및 큐에 추가
-            dp[next_i][next_j] = cost + add_cost
-            q.append((next_i, next_j, cost + add_cost, direction[2]))
-    return answer * 100
+# def solution(board):
+#     n = len(board)
+#     answer = int(1e9)
+#     dp = [[int(1e9) for _ in range(n)] for _ in range(n)]
+#     # 현재 지나간 길을 확인하기 위한 idx 추가
+#     directions = [(-1, 0, 0), (0, 1, 1), (1, 0, 2), (0, -1, 3)]
+#     # i, j, cost, direction
+#     q = deque([(0, 0, 0, -1)])
+#     while q:
+#         i, j, cost, dir_idx = q.popleft()
+#         # 정답 범위이고, 현재 cost가 더 적을 때 값 할당
+#         if (i, j) == (n - 1, n - 1) and answer > cost:
+#             answer = cost
+#         for direction in directions:
+#             # 다음 값 셋팅
+#             next_i = i + direction[0]
+#             next_j = j + direction[1]
+#             add_cost = 1 if dir_idx == direction[2] or dir_idx == -1 else 6
+#             # 현재 값 판단할 지 여부
+#             if not (0 <= next_i < n and 0 <= next_j < n) or board[next_i][next_j] == 1:
+#                 continue
+#             if dp[next_i][next_j] < cost + add_cost:
+#                 continue
+#             # dp에 값 설정 및 큐에 추가
+#             dp[next_i][next_j] = cost + add_cost
+#             q.append((next_i, next_j, cost + add_cost, direction[2]))
+#     return answer * 100
 
 
-solution()
+# solution()
+
+
+n = "X.XXXXX.X."
+
+
+def solution(S):
+    enable_print = S % 10
+    while S > 0:
+        if enable_print >= 0:
+            enable_print = 1
+        if enable_print == 1:
+            print(S % 10, end="")
+        S = S // 10
+
+
+def solution(S):
+    cnt = 0
+    num = len(S) - 1
+    S = list(S)
+    while num >= 0:
+        if S[num] == "X":
+            cnt += 1
+            num -= 3
+        else:
+            num -= 1
+    print(cnt)
+
+
+solution(n)
