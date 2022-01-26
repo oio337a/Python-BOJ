b196477174fe5fbc4b6fb8e5b9d719450d47b4d5
oio337a <oio337a@gmail.com>
2022-01-26 23:28:49 +0900

    PS

--- 1052.py
+++ 1052.py
@@ -42,13 +42,13 @@ bottle[1] = N
 
 liter, buy = 1, 0
 while 1:
-    while liter in bottle: 
+    while liter in bottle:
         #해당 liter을 가진 물병이 1병 이상 존재할 때, 합칠 수 있을 만큼 합침
         if bottle[liter] > 1:
             bottle[liter * 2] += bottle[liter] // 2
             bottle[liter] %= 2
-        liter *= 2 
-        
+        liter *= 2
+
     if sum(list(bottle.values())) <= K: #총 가지고 있는 물병의 양이 K개 이하일 경우 반복문 종료
         break
 
@@ -59,6 +59,6 @@ while 1:
     bottle[liters[1]] = 0
     bottle[liters[1] * 2] += 1
     liter = liters[1] * 2
-        
+
 print(buy)
 """
