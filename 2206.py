17771d8b128d88816a20dd36cf619a67e462eb51
oio337a <oio337a@gmail.com>
2022-02-06 15:45:22 +0900

    PS

--- 2206.py
+++ 2206.py
@@ -6,16 +6,16 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-board = [input().rstrip() for _ in range(n)]
+board = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 move_cnt = 1
 break_cnt = 0
 
-
 def bfs(x, y):
+    global move_cnt, break_cnt
     q = deque()
     q.append([x, y])
     visit = [[0] * m for i in range(n)]
@@ -25,8 +25,14 @@ def bfs(x, y):
         for k in range(4):
             x = a + dx[k]
             y = b + dy[k]
-            if 0 <= x < n and 0 <= y < m and visit[x][y] == 0 and temp[x][y] != 0:
-                temp[x][y] = 0
+            if 0 <= x < n and 0 <= y < m and visit[x][y] == 0:
+                if board[x][y] == 1 and break_cnt == 0:
+                    break_cnt = 1
+                    visit[x][y] = 1
+                elif board[x][y] == 1 and break_cnt == 1:
+                    continue
                 visit[x][y] = 1
                 q.append([x, y])
+                move_cnt += 1
+bfs(0, 0)
 print(move_cnt)
