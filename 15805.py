8ed0ccd53fb31e92d3e59c409fe3398a8230767e
oio337a <oio337a@gmail.com>
2022-02-16 00:02:46 +0900

    PS

--- 15805.py
+++ 15805.py
@@ -1,8 +1,34 @@
-import sys
-input = sys.stdin.readline
+# import sys, copy
+# input = sys.stdin.readline
+#
+# n = int(input())
+# li = list(map(int, input().split()))
+#
+# check_li = [-1 for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         if li[j] == i and j != n - 1:
+#             if check_li[li[j + 1]] == -1:
+#                 check_li[li[j + 1]] = i
+#
+# print(-1, end=' ')
+# print(*check_li[1:max(li)+1])
 
 n = int(input())
 li = list(map(int, input().split()))
+answer = [False for _ in range(200_000)]
 
-for i in range(1, n):
-    print(li[i])
+s = [li[0]]
+answer[li[0]] = -1
+
+for i in range(n):
+    if li[i] in s:
+        continue
+    answer[li[i]] = li[i - 1]
+    s.append(li[i])
+
+cnt_s = len(s)
+
+print(cnt_s)
+print(answer[0:cnt_s])
