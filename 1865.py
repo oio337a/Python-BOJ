d975f8ee073deb7a714a66f391dba337e8e421cb
oio337a <oio337a@gmail.com>
2022-02-18 18:34:29 +0900

    PS

--- 1865.py
+++ 1865.py
@@ -1,6 +1,13 @@
 # 웜홀
 # https://www.acmicpc.net/problem/1865
+"""
+헷갈렸던 부분은 시작점이 1번 지점이 아닐 수도 있다는 점이었는데,
+임의의 한 지점에서 출발을 하므로 단순히 그래프상에
+음의 사이클이 있는지 없는지 확인만 하면 되는 문제로 귀결됩니다.
 
+따라서, 최단 경로를 기록할 리스트의 1번 인덱스에 0으로 초기화할 필요가 없으며,
+벨만 포드 알고리즘에서 현재 노드의 값이 INF(무한대)인지 아닌지 확인하는 조건을 넣으면 안 됩니다.
+"""
 
 import sys
 input = sys.stdin.readline
@@ -10,7 +17,7 @@ def bellman_ford():
 
     for i in range(1, n + 1):
         for j in range(1, n + 1):
-            for w, v in arr[j]:
+            for w, v in arr[j]: #
                 if dist[v] > w + dist[j]:
                     dist[v] = w + dist[j]
                     if i == n:
