3baef074e1987687bdc59c99ffd6efd322a92a2b
oio337a <oio337a@gmail.com>
2022-02-21 22:18:47 +0900

    PS

---
+++ 2252.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = []
+indegree = [0 for _ in range(32001)]
+graph = [[] for i in range(32001)]
+queue = deque()
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    indegree[b] += 1
+    graph[a].append(b)
+
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+while queue:
+    student = queue.popleft()
+    print(student, end=' ')
+    for j in graph[student]:
+        indegree[j] -= 1
+        if indegree[j] == 0:
+            queue.append(j)
