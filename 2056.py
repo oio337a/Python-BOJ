168d4197689bb4a62c6e95fde1dcc2c9b229b9ce
oio337a <oio337a@gmail.com>
2022-02-21 18:27:46 +0900

    PS

--- 2056.py
+++ 2056.py
@@ -1,6 +1,20 @@
+# 작업
+# https://www.acmicpc.net/problem/2056
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-for _ in range(n):
-    
+dp = [0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)] # 선행관계 저장
+for i in range(1, n+1):
+    cost, m, *lst = map(int, input().split())
+    dp[i] = cost
+    for x in lst:
+        graph[i].append(x)
+
+for i in range(1, n+1):
+    tmp = 0
+    for j in graph[i]:
+        tmp = max(tmp, dp[j])
+    dp[i] += tmp
+print(max(dp))
