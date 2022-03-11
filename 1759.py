db9ed13bbeb01fc5b063427224025caa78e92b30
oio337a <oio337a@gmail.com>
2022-03-11 19:02:30 +0900

    PS

---
+++ 1759.py
@@ -0,0 +1,26 @@
+# 암호 만들기
+# https://www.acmicpc.net/problem/1759
+# 브루트 포스
+
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+l, c = map(int, input().split())
+alpha = sorted(input().split())
+mo = ['a', 'e', 'i', 'o', 'u']
+result = []
+
+for c in list(combinations(alpha, l)):
+    mo_cnt = 0
+    ja_cnt = 0
+    for char in c:
+        if char in mo:
+            mo_cnt += 1
+        else:
+            ja_cnt += 1
+    if mo_cnt > 0 and ja_cnt > 1:
+        result.append("".join(c))
+
+for i in result:
+    print(i)
