d072c4a3f09cdd1caeacd0e4fd25d83d18c89929
oio337a <oio337a@gmail.com>
2022-03-13 09:21:03 +0900

    PS

---
+++ (프) 보석 쇼핑.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def solution(gems):
+    answer = []
+    gems_set = set(gems)
+    gems_num = len(gems_set)
+
+    return answer
+
+
+
+
+solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])
