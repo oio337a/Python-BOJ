9e33e784eca4d495a3b60f824565b643af65ec8b
oio337a <oio337a@gmail.com>
2024-02-15 15:08:01 +0900

    1269 clear

--- src/1269.py
+++ src/1269.py
@@ -1,14 +1,30 @@
 
-import sys
+# import sys
 
+# input = sys.stdin.readline
+
+# a, b = map(int, input().split())
+
+# li_1 = set(list(map(int, input().split())))
+# li_2 = set(list(map(int, input().split())))
+
+# answer_1 = li_1 - li_2
+# answer_2 = li_2 - li_1
+
+# print(len(answer_1)+len(answer_2))
+
+import sys
+from collections import Counter
 input = sys.stdin.readline
 
 a, b = map(int, input().split())
+l = list(map(int, input().split()))
+l += map(int, input().split())
+cnt = Counter(l)
 
-li_1 = set(list(map(int, input().split())))
-li_2 = set(list(map(int, input().split())))
+ans = 0
+for i in cnt:
+  if cnt[i] == 1:
+    ans += 1
 
-answer_1 = li_1 - li_2
-answer_2 = li_2 - li_1
-
-print(len(answer_1)+len(answer_2))
+print(ans)
